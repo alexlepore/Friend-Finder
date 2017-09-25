7401c33c1aaa60ace52913fae278643a3ea2be35
unknown <alepore@umass.edu>
2017-09-25 17:53:25 -0400

    1st

---
+++ app/data/friend.js
@@ -0,0 +1,44 @@
+var friends = [ {
+  "name" : "Sam",
+  "photo" : "https://i.pinimg.com/736x/31/6b/91/316b916f0a2ffb7b8b0b4495ed8cfd95--skins-uk-larissa.jpg",
+  "scores" : [4,5,1,4,3,4,2,2,3,1]
+}, {
+  "name" : "Anthony",
+  "photo" : "https://upload.wikimedia.org/wikipedia/en/7/70/Tony_Stonem.jpg",
+  "scores" : [3,2,4,4,5,3,3,5,4,1]
+}, {
+  "name" : "Calvin",
+  "photo" : "http://www.skinsmusic.co.uk/images/season_3/freddie/freddie_skins_close.png",
+  "scores" : [3,1,3,4,4,3,3,4,2,2]
+}, {
+  "name" : "Sarah",
+  "photo" : "http://vignette2.wikia.nocookie.net/enskins/images/d/d2/Gracie_Promo_pics_serie6.jpg/revision/latest?cb=20111228163831",
+  "scores" : [2,5,2,3,3,2,2,4,2,5]
+}, {
+  "name" : "Juan",
+  "photo" : "http://www.skinsmusic.co.uk/images/Season_6/Alex_skins_season6.jpg",
+  "scores" : [2,5,2,2,1,1,2,4,4,2]
+}, {
+  "name" : "Xavier",
+  "photo" : "https://upload.wikimedia.org/wikipedia/en/b/b1/Sid_4.jpg",
+  "scores" : [2,5,3,1,3,5,3,1,5,1]
+}, {
+  "name" : "Tanner",
+  "photo" : "https://upload.wikimedia.org/wikipedia/it/0/0b/Maxxie_Oliver.png",
+  "scores" : [2,3,5,5,5,2,5,4,5,3]
+}, {
+  "name" : "Michelle",
+  "photo" : "https://upload.wikimedia.org/wikipedia/en/2/29/Michelle_skins.jpg",
+  "scores" : [3,2,4,5,3,4,1,2,5,1]
+}, {
+  "name" : "Lucas",
+  "photo" : "https://upload.wikimedia.org/wikipedia/it/thumb/6/6f/Alo_Creevey.png/260px-Alo_Creevey.png",
+  "scores" : [2,1,3,3,4,5,2,4,1,5]
+}, {
+  "name" : "Lynn",
+  "photo" : "https://metrouk2.files.wordpress.com/2012/10/article-1350401794706-15882a14000005dc-1684_466x339.jpg",
+  "scores" : [5,3,4,3,5,3,3,3,5,1]
+}];
+
+
+module.exports = friends;
